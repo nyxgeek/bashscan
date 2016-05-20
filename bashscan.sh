9fbce95b1187febf4b314835b95d1906abef62f6
nyxgeek <nyxgeek@gmail.com>
2016-05-20 00:51:23 -0500

    minor tweaks

--- bashscan.sh
+++ bashscan.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+	echo "Usage: ./bashscan.sh 127.0.0.1"
+	exit 1
+fi
+
+echo "Starting TCP portscan of $1"
+
 for i in `seq 1 1025`; do
 	nc -v -w1 -z $1 $i 2>/dev/null
 	if [ $? -eq 0 ]; then
